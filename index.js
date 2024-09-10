58008a910809c57afd1ecf7d2a1ea6cd3bfc0b2d
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2024-09-10 10:57:57 +0530

    Create API calls for login and home page

--- index.js
+++ index.js
@@ -8,7 +8,86 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
+headers = {
+  accept: "application/json",
+  Authorization:
+    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMGNhYjAxOTkyYThiM2ZlOWU3MGE3Y2I4MzcxNTU3NiIsIm5iZiI6MTcyNTcxMjQ4NC45OTgwNTMsInN1YiI6IjY2ZDlkNTFmYjllOWEzODFlOTZkMDlkOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uMnqTkdsj9-tuzPhKWIOJp1TviqUWt71rbn3dZ8drbU",
+};
+
+// for login page
+app.get("/", (req, res) => {
+  const options = {
+    method: "GET",
+    url: "https://api.themoviedb.org/3/discover/movie",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMGNhYjAxOTkyYThiM2ZlOWU3MGE3Y2I4MzcxNTU3NiIsIm5iZiI6MTcyNTcxMjQ4NC45OTgwNTMsInN1YiI6IjY2ZDlkNTFmYjllOWEzODFlOTZkMDlkOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uMnqTkdsj9-tuzPhKWIOJp1TviqUWt71rbn3dZ8drbU",
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      movies = response.data.results;
+      res.render("login", movies);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+});
+
+// for home page
+app.get("/home", (req, res) => {
+  const options = {
+    method: "GET",
+    url: "https://api.themoviedb.org/3/movie/top_rated",
+    params: { language: "en-US", page: "1" },
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMGNhYjAxOTkyYThiM2ZlOWU3MGE3Y2I4MzcxNTU3NiIsIm5iZiI6MTcyNTcxMjQ4NC45OTgwNTMsInN1YiI6IjY2ZDlkNTFmYjllOWEzODFlOTZkMDlkOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uMnqTkdsj9-tuzPhKWIOJp1TviqUWt71rbn3dZ8drbU",
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      trenmovies = response.data.results;
+      console.log(trenmovies);
+
+      const options = {
+        method: "GET",
+        url: "https://api.themoviedb.org/3/discover/movie",
+        params: {
+          include_adult: "false",
+          include_video: "false",
+          language: "en-US",
+          page: "1",
+          sort_by: "popularity.desc",
+        },
+        headers: {
+          accept: "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMGNhYjAxOTkyYThiM2ZlOWU3MGE3Y2I4MzcxNTU3NiIsIm5iZiI6MTcyNTcxMjQ4NC45OTgwNTMsInN1YiI6IjY2ZDlkNTFmYjllOWEzODFlOTZkMDlkOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uMnqTkdsj9-tuzPhKWIOJp1TviqUWt71rbn3dZ8drbU",
+        },
+      };
+
+      axios
+        .request(options)
+        .then(function (response) {
+          popmovies = response.data.results;
+          res.render("home", popmovies);
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+});
 
 app.listen(3000, () => {
-    console.log("LISTENING ON PORT 3000");
+  console.log("LISTENING ON PORT 3000");
 });
