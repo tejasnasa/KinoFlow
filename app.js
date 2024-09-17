1d943a3f26b9928084ee64cb24e70343702256ad
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2024-09-17 18:26:42 +0530

    Deploy

--- app.js
+++ app.js
@@ -110,6 +110,7 @@ app.get("/genre/:gen", async (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => {
-  console.log("LISTENING ON PORT 3000");
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`LISTENING ON PORT ${port}`);
 });
